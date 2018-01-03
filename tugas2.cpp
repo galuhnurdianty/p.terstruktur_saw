3757df0d3396c60161aa8cf3bb0d0434e0594955
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-01-03 20:18:59 +0800

    tampilan pertama matriks x

--- tugas2.cpp
+++ tugas2.cpp
@@ -5,5 +5,12 @@ using namespace std;
 int main(){
 	cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
 	double a1c[5]={4,4,5,3,3}, a2c[5]={3,3,4,2,3}, a3c[5]={5,4,2,2,2};
+	
+	cout << "MATRIX X :"<<endl;
+	int pertama1=0;
+	do{
+		cout << a1c[pertama1]<<" | ";
+		pertama1++;
+	}
 }
 
