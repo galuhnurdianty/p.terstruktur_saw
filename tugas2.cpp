68c6e1b190d9d98e0b803d1656fe35cec80475d0
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-01-03 20:35:13 +0800

    variabel penjumlahan rw

--- tugas2.cpp
+++ tugas2.cpp
@@ -74,5 +74,9 @@ int main(){
 		cout << RWa3c[ketiga3]<<" | ";
 		ketiga3++;
 	}while(ketiga3<5);cout<<endl;
+	
+	double ha1[5]={ RWa1c[0]+RWa1c[1]+RWa1c[2]+RWa1c[3]+RWa1c[4] };
+	double ha2[5]={ RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4] };
+	double ha3[5]={ RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4] };	
 }
 
