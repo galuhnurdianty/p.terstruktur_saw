54c2e3a0c5a0c511f4b5825bc28940eac5992d24
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-01-03 19:48:03 +0800

    deklarasi variabel
    menentukan variabel x, r, w, maxc

---
+++ tugas1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	//DEKLARASI VARIABEL
+	double x[3][5]= {x[0][1],x[2][4]};
+	double r[3][5]= {x[0][1],x[2][4]};
+	double rw[3][5]= {x[0][1],x[2][4]};
+		
+	double w1,w2,w3,w4,w5;
+	double maxc [5]= {maxc[0],maxc[4]};
+	double ha1,ha2,ha3;
+}
